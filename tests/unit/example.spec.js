0b632dd646840c8b733831ad4d47b9d44a23ee58
duyuanzhe <duyuanzhe@chupinxiu.com>
2019-12-03 16:58:02 +0800

    x

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -6,20 +6,6 @@ import { expect } from "chai";
 
 describe("App/index.vue", () => {
   it("expect(Number(2).to.equal(2)", () => {
-    // //获取组件实例
-    // const Constructor = Vue.extend(App);
-    // //挂载组件
-    // const vm = new Constructor().$mount();
-    // console.log('count >>> ', vm.count)
-    // //获取button
-    // const button = vm.$el.querySelector('button');
-    // //新建点击事件
-    // const clickEvent = new window.Event('click');
-    // //触发点击事件
-    // button.dispatchEvent(clickEvent);
-    // //监听点击事件
-    // vm._watcher.run();
-    // 断言
     expect(Number(2)).to.equal(2)
-  });
-});
+  })
+})
