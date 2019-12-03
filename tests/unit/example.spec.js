04459f49dd861012d8f4ef349abbfd639b1a4843
duyuanzhe <duyuanzhe@chupinxiu.com>
2019-12-03 16:44:18 +0800

    github pages

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,18 +1,25 @@
 import { expect } from "chai";
-import { shallowMount } from "@vue/test-utils";
-import MyUploadImg from "@/components/MyUploadImg";
+// import Vue from 'vue'
+// import { shallowMount } from "@vue/test-utils";
+// import MyUploadImg from "@/components/MyUploadImg/index.vue";
+// import App from "@/App.vue";
 
-describe("MyUploadImg/index.vue", () => {
-  it("renders props.msg when passed", () => {
-    // eslint-disable-next-line camelcase
-    const img_list = [{ url: "" }, { url: "" }, { url: "" }];
-    const wrapper = shallowMount(MyUploadImg, {
-      propsData: { img_list }
-    });
-    console.log("HHHHHHHHHHHHHHHHHHHHH \n \n \n");
-    console.log(wrapper);
-    expect(".upload-img-box")
-      .length()
-      .equal(3);
+describe("App/index.vue", () => {
+  it("expect(Number(2).to.equal(2)", () => {
+    // //获取组件实例
+    // const Constructor = Vue.extend(App);
+    // //挂载组件
+    // const vm = new Constructor().$mount();
+    // console.log('count >>> ', vm.count)
+    // //获取button
+    // const button = vm.$el.querySelector('button');
+    // //新建点击事件
+    // const clickEvent = new window.Event('click');
+    // //触发点击事件
+    // button.dispatchEvent(clickEvent);
+    // //监听点击事件
+    // vm._watcher.run();
+    // 断言
+    expect(Number(2)).to.equal(2)
   });
 });
